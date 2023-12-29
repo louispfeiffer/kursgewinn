5041f5211b48af44561d70e2c2f4d11b3af11795
louispfeiffer <pfeifferlouis2006@gmail.com>
2023-12-29 13:32:20 +0100

    Impressum Update

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -42,55 +42,55 @@ const ImpressumPage = () => {
                 Disclaimer: Keine Anlageberatung oder Kaufempfehlung!
 
                 Alle hier angegebenen Informationen stellen keine Anlageberatung oder Kaufempfehlung dar!
-                Es handelt sich bei allen Aussagen um meine allgemein veröffentlichte Meinung. 
+                Es handelt sich bei allen Aussagen um meine allgemein veröffentlichte Meinung.
                 Die Informationen habe ich aus meinen Erfahrungen als Privatanleger gewonnen.
                 Die Informationen sind nicht auf deine persönliche Situation zugeschnitten und sind daher kein Ersatz für
                 eine professionelle und individuelle Beratung durch hierfür qualifizierte Personen.
                 Diese Website kann und will keine persönliche Anlageberatung durch Profis ersetzen.
-                Zudem können Fehler in den vorhandenen Informationen sein, 
-                sodass eine allumfassende Wissensvermittlung zum Thema ETF allein durch diese Anleitung nicht sichergestellt werden kann. 
+                Zudem können Fehler in den vorhandenen Informationen sein,
+                sodass eine allumfassende Wissensvermittlung zum Thema ETF allein durch diese Anleitung nicht sichergestellt werden kann.
                 Alle Informationen dienen zur Entscheidungshilfe und stellen keine Anlagenberatung dar!
-                 Ich übernehme keinerlei Haftung für deine Anlageentscheidungen.
+                Ich übernehme keinerlei Haftung für deine Anlageentscheidungen.
 
                 Risikohinweis
-                
-                Die Wertentwicklung in der Vergangenheit stellt keinen zuverlässigen Indikator für die zukünftige Entwicklung dar. 
+
+                Die Wertentwicklung in der Vergangenheit stellt keinen zuverlässigen Indikator für die zukünftige Entwicklung dar.
                 Aktien und Fonds können fallen, selbst ein Totalverlust ist nicht ausgeschlossen,
                 sie können aber auch steigen.
-                Bei Auslandsaktien können Währungseinflüsse die Performance verbessern oder verschlechtern. 
+                Bei Auslandsaktien können Währungseinflüsse die Performance verbessern oder verschlechtern.
                 Daher solltest du dich intensiv mit dem Thema Geldanlage auseinandersetzen und noch weitere Quellen sowie eine professionelle Beratung nutzen.
 
                 Haftungsausschluss – Disclaimer:
 
                 Haftung für Inhalte
 
-                Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und nach bestem Gewissen erstellt. 
-                Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. 
-                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen 
-                Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, 
-                übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, 
-                die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der 
+                Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und nach bestem Gewissen erstellt.
+                Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
+                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+                Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+                übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen,
+                die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
                 Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
-                Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt 
-                der Kenntniserlangung einer konkreten Rechtsverletzung möglich. 
+                Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt
+                der Kenntniserlangung einer konkreten Rechtsverletzung möglich.
                 Bei Bekanntwerden von den o.g. Rechtsverletzungen werden wir diese Inhalte unverzüglich entfernen.
 
                 Haftungsbeschränkung für externe Links
 
-                Unsere Webseite enthält Links auf externe Webseiten Dritter. 
-                Auf die Inhalte dieser direkt oder indirekt verlinkten Webseiten haben wir keinen Einfluss. 
-                Daher können wir für die „externen Links“ auch keine Gewähr auf Richtigkeit der Inhalte übernehmen. 
+                Unsere Webseite enthält Links auf externe Webseiten Dritter.
+                Auf die Inhalte dieser direkt oder indirekt verlinkten Webseiten haben wir keinen Einfluss.
+                Daher können wir für die „externen Links“ auch keine Gewähr auf Richtigkeit der Inhalte übernehmen.
                 Für die Inhalte der externen Links sind die jeweilige Anbieter oder Betreiber (Urheber) der Seiten verantwortlich.
-                Die externen Links wurden zum Zeitpunkt der Linksetzung auf eventuelle Rechtsverstöße überprüft 
-                und waren im Zeitpunkt der Linksetzung frei von rechtswidrigen Inhalten. Eine ständige inhaltliche 
-                Überprüfung der externen Links ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht möglich. 
-                Bei direkten oder indirekten Verlinkungen auf die Webseiten Dritter, die außerhalb unseres Verantwortungsbereichs liegen, 
-                würde eine Haftungsverpflichtung ausschließlich in dem Fall nur bestehen, wenn wir von den Inhalten Kenntnis erlangen und 
+                Die externen Links wurden zum Zeitpunkt der Linksetzung auf eventuelle Rechtsverstöße überprüft
+                und waren im Zeitpunkt der Linksetzung frei von rechtswidrigen Inhalten. Eine ständige inhaltliche
+                Überprüfung der externen Links ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht möglich.
+                Bei direkten oder indirekten Verlinkungen auf die Webseiten Dritter, die außerhalb unseres Verantwortungsbereichs liegen,
+                würde eine Haftungsverpflichtung ausschließlich in dem Fall nur bestehen, wenn wir von den Inhalten Kenntnis erlangen und
                 es uns technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern.
-                Diese Haftungsausschlusserklärung gilt auch innerhalb des eigenen Internetauftrittes „Name Ihrer Domain“ 
-                gesetzten Links und Verweise von Fragestellern, Blogeinträgern, Gästen des Diskussionsforums. Für illegale, 
-                fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung 
-                solcherart dargestellten Informationen entstehen, haftet allein der Diensteanbieter der Seite, 
+                Diese Haftungsausschlusserklärung gilt auch innerhalb des eigenen Internetauftrittes „Name Ihrer Domain“
+                gesetzten Links und Verweise von Fragestellern, Blogeinträgern, Gästen des Diskussionsforums. Für illegale,
+                fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung
+                solcherart dargestellten Informationen entstehen, haftet allein der Diensteanbieter der Seite,
                 auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.
                 Werden uns Rechtsverletzungen bekannt, werden die externen Links durch uns unverzüglich entfernt.
 
@@ -99,6 +99,53 @@ const ImpressumPage = () => {
                 Die auf unserer Webseite veröffentlichen Inhalte und Werke unterliegen dem deutschen Urheberrecht (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf) . Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung des geistigen Eigentums in ideeller und materieller Sicht des Urhebers außerhalb der Grenzen des Urheberrechtes bedürfen der vorherigen schriftlichen Zustimmung des jeweiligen Urhebers i.S.d. Urhebergesetzes (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf ). Downloads und Kopien dieser Seite sind nur für den privaten und nicht kommerziellen Gebrauch erlaubt. Sind die Inhalte auf unserer Webseite nicht von uns erstellt wurden, sind die Urheberrechte Dritter zu beachten. Die Inhalte Dritter werden als solche kenntlich gemacht. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte unverzüglich entfernen.
                 Dieses Impressum wurde freundlicherweise von jurarat.de zur Verfügung gestellt.
             </p>
+
+            <div class="container mx-auto px-4">
+                <h1 class="text-xl font-bold text-center my-5">Impressum</h1>
+                <div class="bg-white p-6 rounded-lg shadow-md">
+                    <div class="mb-4">
+                        <h2 class="text-lg font-semibold">Angaben gem. § 5 TMG:</h2>
+                        <p>
+                            Louis Pfeiffer
+                            Birkbuschgarten 2
+                            12167, Berlin
+                        </p>
+
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Kontaktaufnahme:</h2>
+                            <p>Telefon: 0176 72782147<br></br>E-Mail: <a href="mailto:louispfeiffer@info.com" class="text-blue-500">louispfeiffer@info.com</a></p>
+                        </div>
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Umsatzsteuer-ID</h2>
+                            <p>Umsatzsteuer-Identifikationsnummer gem. § 27 a Umsatzsteuergesetz:<br></br>DE XXX XXX XXX</p>
+                        </div>
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Disclaimer: Keine Anlageberatung oder Kaufempfehlung!</h2>
+                            <p>Alle hier angegebenen Informationen stellen keine Anlageberatung oder Kaufempfehlung dar! Es handelt sich bei allen Aussagen um meine allgemein veröffentlichte Meinung. Die Informationen habe ich aus meinen Erfahrungen als Privatanleger gewonnen. Die Informationen sind nicht auf deine persönliche Situation zugeschnitten und sind daher kein Ersatz für eine professionelle und individuelle Beratung durch hierfür qualifizierte Personen. Diese Website kann und will keine persönliche Anlageberatung durch Profis ersetzen. Zudem können Fehler in den vorhandenen Informationen sein, sodass eine allumfassende Wissensvermittlung zum Thema ETF allein durch diese Anleitung nicht sichergestellt werden kann. Alle Informationen dienen zur Entscheidungshilfe und stellen keine Anlagenberatung dar! Ich übernehme keinerlei Haftung für deine Anlageentscheidungen.</p>
+                        </div>
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Risikohinweis</h2>
+                            <p>Die Wertentwicklung in der Vergangenheit stellt keinen zuverlässigen Indikator für die zukünftige Entwicklung dar. Aktien und Fonds können fallen, selbst ein Totalverlust ist nicht ausgeschlossen, sie können aber auch steigen. Bei Auslandsaktien können Währungseinflüsse die Performance verbessern oder verschlechtern. Daher solltest du dich intensiv mit dem Thema Geldanlage auseinandersetzen und noch weitere Quellen sowie eine professionelle Beratung nutzen.</p>
+                        </div>
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Haftungsausschluss – Disclaimer:</h2>
+                            <p>Haftung für Inhalte<br>Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und nach bestem Gewissen erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. ...
+                                [Weiterer Text des Haftungsausschlusses] ...</p>
+                        </div>
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Haftungsbeschränkung für externe Links</h2>
+                            <p>Unsere Webseite enthält Links auf externe Webseiten Dritter. ...
+                                [Weiterer Text zur Haftungsbeschränkung] ...</p>
+                        </div>
+                        <div class="mb-4">
+                            <h2 class="text-lg font-semibold">Urheberrecht</h2>
+                            <p>Die auf unserer Webseite veröffentlichen Inhalte und Werke unterliegen dem deutschen Urheberrecht. ...
+                                [Weiterer Text zum Urheberrecht] ...</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
 
 
 
@@ -109,4 +156,4 @@ const ImpressumPage = () => {
 
 export default ImpressumPage
 
-export const Head = () => <title>Schreib mir!</title>
+export const Head = () => <title>Impressum</title>
