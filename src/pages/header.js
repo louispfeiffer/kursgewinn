eed0ac6ba49152b8a51bfb18fcb9e6f97ff28c8b
louispfeiffer <pfeifferlouis2006@gmail.com>
2024-01-05 20:41:40 +0100

    Head Update

--- src/pages/header.js
+++ src/pages/header.js
@@ -7,9 +7,11 @@ function Header() {
     return (
 
         <head>
-            <meta property="og:title" content="Kursgewinn-Finanzen" />
-            <meta property="og:description" content="Unser Team hilft dir, deine Finanzen zu kontrollieren" />
-            <meta name="description" content="Unser Team hilft dir, deine Finanzen zu kontrollieren"></meta>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <meta name="description" content="Entdecke bei Kursgewinn, wie du durch gezielte Investitionen finanzielle Freiheit erlangen kannst. Unser Expertenteam bietet dir umfassende Beratung, 24/7 Support und maßgeschneiderte Finanzlösungen, um deine Ziele zu erreichen. Werde Teil unseres Netzwerks und profitiere von unserem umfangreichen Wissen und Kontakten. Beginne jetzt deine Reise zur finanziellen Unabhängigkeit." />
+            <meta name="keywords" content="Finanzberatung, Investitionsstrategien, Vermögensaufbau, Finanzmanagement, 24/7 Support, Finanzielle Freiheit, Rendite, Netzwerkbildung, Wissensaustausch, Vermögensverwaltung, Finanzplanung, Wirtschaftswachstum, Investition in die Zukunft, Finanzen kontrollieren, Finanzziele, Rendite maximieren, Investitionsberatung, Kursgewinn, Finanzberater" />
         </head>
 
 
