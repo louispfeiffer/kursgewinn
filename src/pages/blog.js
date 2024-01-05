0199c6688bcff015c821e6c944b6949e69ff9bc1
louispfeiffer <pfeifferlouis2006@gmail.com>
2024-01-05 23:53:29 +0100

    lost

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -42,7 +42,7 @@ const BlogPage = () => {
                         <div class="relative w-full">
                             <img class="w-full rounded-lg drop-shadow-2xl" src={AppleHeadQ} alt="Hier ist ein Bild" />
                             <div class="absolute top-0 left-0 w-full h-full bg-black hover:bg-opacity-0 bg-opacity-20 rounded-lg flex items-center justify-center">
-                                <div class="text-white px-4 py-2 text-center">
+                                <div class="text-black px-4 py-2 text-center">
                                     Kurzer Call to Action für den Blog
                                 </div>
                             </div>
@@ -51,7 +51,7 @@ const BlogPage = () => {
                         <div class="relative w-full">
                             <img class="w-full rounded-lg drop-shadow-2xl" src={Nvidia} alt="Hier ist ein Bild"></img>
                             <div class="absolute top-0 left-0 w-full h-full bg-black hover:bg-opacity-0 bg-opacity-20 rounded-lg flex items-center justify-center">
-                                <div class="text-white px-4 py-2 text-center">
+                                <div class="text-black px-4 py-2 text-center">
                                     Kurzer Call to Action für den Blog
                                 </div>
                             </div>
@@ -60,7 +60,7 @@ const BlogPage = () => {
                         <div class="relative w-full">
                             <img class="w-full rounded-lg drop-shadow-2xl" src={Disney} alt="Hier ist ein Bild"></img>
                             <div class="absolute top-0 left-0 w-full h-full bg-black hover:bg-opacity-0 bg-opacity-20 rounded-lg flex items-center justify-center">
-                                <div class="text-white px-4 py-2 text-center">
+                                <div class="text-black px-4 py-2 text-center">
                                     Kurzer Call to Action für den Blog
                                 </div>
                             </div>
